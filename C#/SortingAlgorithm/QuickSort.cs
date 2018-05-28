2ea07739bcd30e29bbe8df1cad2ec98a51c4a5a4
wuguoxian <104002782@qq.com>
2018-05-28 15:25:58 +0800

    优化快排

--- C#/SortingAlgorithm/QuickSort.cs
+++ C#/SortingAlgorithm/QuickSort.cs
@@ -13,25 +13,41 @@ namespace SortingAlgorithm
     {
         public static void Sort(int[] arr,int startIndex, int endIndex)
         {
-            if (endIndex <= startIndex)
+            if (startIndex < endIndex)
             {
-                return;
+                //分区操作
+                int pivotIndex = Partition(arr, startIndex, endIndex);
+                //将其他未排序的排好
+                Sort(arr, startIndex, pivotIndex - 1);
+                Sort(arr, pivotIndex + 1, endIndex);
             }
+        }
 
-            int i = startIndex; //用于遍历arr[startIndex -> endIndex]
-            int p = startIndex; //循环结束pivot应该替换的位置,循环未结束，则是较大arr的开头
-            int pivot = arr[endIndex];//需要比较的值,不同的快排此处可能有所不同
-            for (; i < endIndex; i++)
+
+        /// <summary>
+        /// 分区Partition：
+        ///     找到一个pivot,比它小的放到arr前面，比他大的放到arr后面，最后将pivot放到指定位置，一轮下来最少排好一个数字
+        /// </summary>
+        public static int Partition(int[] arr, int startIndex, int endIndex)
+        {
+            //循环结束pivot应该替换的位置,循环未结束，则是较大arr的开头
+            int pivotIndex = startIndex;
+            //需要比较的值,不同的快排此处可能有所不同
+            int pivotVal = arr[endIndex];
+
+            for (int i = startIndex; i < endIndex; i++)
             {
-                if (arr[i] < pivot)
+                if (arr[i] < pivotVal)
                 {
-                    Util.Swap(arr,i,p);
-                    p++;
+                    Util.Swap(arr, i, pivotIndex);
+                    pivotIndex++;
                 }
             }
-            Util.Swap(arr, endIndex, p);
-            Sort(arr, startIndex, p - 1);
-            Sort(arr, p + 1, endIndex);
+
+            //将pivot放到指定位置
+            Util.Swap(arr, endIndex, pivotIndex);
+
+            return pivotIndex;
         }
     }
 }
