178fdb3c75fd06bffcb242b47c92ba2f3fa4c8be
wuguoxian <104002782@qq.com>
2018-05-29 14:40:52 +0800

    快乐小时排序

--- C#/SortingAlgorithm/BubbleSort.cs
+++ C#/SortingAlgorithm/BubbleSort.cs
@@ -8,16 +8,59 @@ namespace SortingAlgorithm
 {
     public class BubbleSort
     {
-        public static void Sort(int[] arr) {
+        /// <summary>
+        /// 普通冒泡排序
+        /// </summary>
+        public static void Sort(int[] arr)
+        {
             for (int i = 0; i < arr.Length; i++)
             {
                 for (int j = 0; j < arr.Length - i - 1; j++)
                 {
-                    if(arr[j] > arr[j+1])
+                    if (arr[j] > arr[j + 1])
                     {
                         Util.Swap(arr, j, j + 1);
                     }
                 }
+            }
+        }
+
+        /// <summary>
+        /// 鸡尾酒排序,定向冒泡排序,是冒泡排序的一种变形
+        /// </summary>
+        /// <param name="arr"></param>
+        public static void CocktailSort(int[] arr)
+        {
+            bool swaped = true;
+            int rightIndex = 0;
+            int leftIndex = arr.Length - 1;
+
+            while (swaped && leftIndex > rightIndex)
+            {
+                swaped = false;
+
+                for (int j = rightIndex; j < leftIndex ; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        Util.Swap(arr, j, j + 1);
+                        swaped = true;
+                    }
+                }
+
+                leftIndex--;
+
+
+                for (int j = leftIndex; j >= rightIndex + 1; j--)
+                {
+                    if (arr[j] < arr[j - 1])
+                    {
+                        Util.Swap(arr, j, j - 1);
+                        swaped = true;
+                    }
+                }
+
+                rightIndex++;
             }
         }
     }
--- C#/SortingAlgorithmTests/BubbleSortTests.cs
+++ C#/SortingAlgorithmTests/BubbleSortTests.cs
@@ -23,5 +23,18 @@ namespace SortingAlgorithm.Tests
                 Assert.IsTrue(Util.CompareList(originData, sortedData));
             }
         }
+
+        [TestMethod()]
+        public void CocktailSortTest()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                int[] originData = Util.GenRandomIntArr();
+                int[] sortedData = (int[])originData.Clone();
+                Array.Sort(sortedData);
+                BubbleSort.CocktailSort(originData);
+                Assert.IsTrue(Util.CompareList(originData, sortedData));
+            }
+        }
     }
 }
