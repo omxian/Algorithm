69272c1efc790acdf40e921c2551bb11af063634
Owen <104002782@qq.com>
2019-01-26 17:16:05 +0800

    欧几里德算法(辗转相除法)，求最大公因子

---
+++ Lua/EuclideanAlgorithm.lua
@@ -0,0 +1,11 @@
+--欧几里德算法，求最大公因子
+local function EuclideanAlgorithm(n,m)
+  r = n%m
+  if(r == 0) then
+    return m
+  else
+    return EuclideanAlgorithm(m,r)
+  end
+end
+
+assert(EuclideanAlgorithm(1997,615) == 1)
