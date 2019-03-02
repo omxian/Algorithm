406e3d59a26265dc3b79890702f994fc43a3d9e4
Owen <104002782@qq.com>
2019-03-02 18:38:40 +0800

    kmp算法

---
+++ C#/Algorithm/KMPAlgorithm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    public class KMPAlgorithm
+    {
+        public int KMP(string main, string pattern)
+        {
+            List<int> next = GetNext(pattern);
+
+            int i = 0, j = 0;
+
+            while (i < main.Length && j < pattern.Length)
+            {
+                if (j == -1 || main[i] == pattern[j])
+                {
+                    i++;
+                    j++;
+                }
+                else
+                {
+                    j = next[j];
+                }
+            }
+
+            return j == pattern.Length ? i - j : -1;
+        }
+
+        //其实执行的就是自己和自己错一位比较
+        //计算出最长共同子串
+        public List<int> GetNext(string pattern)
+        {
+            //出于编程方便原因，首位设置为-1
+            List<int> next = new List<int>() { -1, 0 };
+
+            int j = 0;
+            //没有必要判断完，后面的一位没有用的
+            for (int i = 1; i < pattern.Length - 1; i++)
+            {
+                if (pattern[i] == pattern[j])
+                {
+                    j++;
+                    next.Add(j);
+                }
+                else
+                {
+                    j = 0;
+                    next.Add(j);
+                }
+            }
+
+            return next;
+        }
+    }
+}
---
+++ C#/AlgorithmTests1/KMPAlgorithmTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Tests
+{
+    [TestClass()]
+    public class KMPAlgorithmTests
+    {
+        [TestMethod()]
+        public void GetNextTest()
+        {
+            KMPAlgorithm kmp = new KMPAlgorithm();
+            Assert.IsTrue(Util.CompareList(kmp.GetNext("abababca"), new List<int> { -1, 0, 0, 1, 2, 3, 4, 0 }));
+        }
+
+        [TestMethod()]
+        public void KMPTest()
+        {
+            KMPAlgorithm kmp = new KMPAlgorithm();
+            Assert.AreEqual("ababc".IndexOf("a"), kmp.KMP("ababc", "a"));
+            Assert.AreEqual("abaacabaaabaab".IndexOf("abaab"), kmp.KMP("abaacabaaabaab", "abaab"));
+            Assert.AreEqual("ababaababc".IndexOf("ababc"), kmp.KMP("ababaababc", "ababc"));
+            Assert.AreEqual("asdfsdf".IndexOf("tewtwe"), kmp.KMP("asdfsdf", "tewtwe"));
+            Assert.AreEqual("ababsssaababc".IndexOf("sssa"), kmp.KMP("ababsssaababc", "sssa"));
+            Assert.AreEqual("a".IndexOf("b"), kmp.KMP("a", "b"));
+            Assert.AreEqual("a".IndexOf("a"), kmp.KMP("a", "a"));
+        }
+    }
+}
